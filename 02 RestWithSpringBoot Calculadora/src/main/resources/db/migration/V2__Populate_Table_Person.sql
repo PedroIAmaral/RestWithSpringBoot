80862365bdd706065d52688cb59351491e493a7b
PedroIAmaral <pedrobrandao.amaral@gmail.com>
2021-01-16 02:24:52 -0400

    Adicionando Migrations no projeto

---
+++ 02 RestWithSpringBoot Calculadora/src/main/resources/db/migration/V2__Populate_Table_Person.sql
@@ -0,0 +1,5 @@
+INSERT INTO `person` (`id`, `adress`, `first_name`, `gener`, `last_name`) VALUES
+	(1, 'Campão', 'Pedro', 'Male', 'Amaral'),
+	(2, 'Santos', 'Gabriel', 'Male', 'Menezes'),
+	(3, 'Santos', 'Tiburcio', 'Male', 'Menezes'),
+	(5, 'Santos', 'Tunico', 'Male', 'do Júlio');
