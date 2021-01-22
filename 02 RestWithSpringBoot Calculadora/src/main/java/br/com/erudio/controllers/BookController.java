e9ad2ca56fd42a73e96edd09c6db0417fce04d32
PedroIAmaral <pedrobrandao.amaral@gmail.com>
2021-01-22 01:52:19 -0400

    Criação do manter book, configuração HATEOAS, Adição do Swagger, Configuração do CORS

--- 02 RestWithSpringBoot Calculadora/src/main/java/br/com/erudio/controllers/BookController.java
+++ 02 RestWithSpringBoot Calculadora/src/main/java/br/com/erudio/controllers/BookController.java
@@ -4,7 +4,6 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -14,68 +13,64 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import br.com.erudio.data.vo.v1.PersonVO;
-import br.com.erudio.services.PersonServices;
-import io.swagger.annotations.Api;
+import br.com.erudio.data.vo.v1.BookVO;
+import br.com.erudio.services.BookServices;
 import io.swagger.annotations.ApiOperation;
 
-@CrossOrigin
-@Api(value = "person Endpoint", description = "Descricaod de pessoa", tags = "personendpoint ")
 @RestController
-@RequestMapping("/api/person/v1")
-public class PersonController {
+@RequestMapping("/api/book/v1")
+public class BookController {
 
 	@Autowired
-	private PersonServices personService;
+	private BookServices bookService;
 	
-	@CrossOrigin(origins="http://localhost:8080")
-	@ApiOperation(value = "Find all pepoples recorded")
+	@ApiOperation(value = "Find all books recorded")
 	@GetMapping(produces = {"application/json","application/xml", "application/x-yaml"})
-	public List<PersonVO> findByAll() {
+	public List<BookVO> findByAll() {
 		
-		return personService.findAll();
+		return bookService.findAll();
 		
 	}
 
-	@ApiOperation(value = "Find people by id")
+	@ApiOperation(value = "Find books by id")
 	@GetMapping(value = "/{id}" , produces = {"application/json","application/xml", "application/x-yaml"})
-	public PersonVO findById(@PathVariable("id") Long id) {
-		PersonVO personVO = personService.findById(id);
-//		personVO.add(linkTo(methodOn(PersonController.class).findById(id)).withSelfRel());
-		return personVO;
+	public BookVO findById(@PathVariable("id") Long id) {
+		BookVO bookVO = bookService.findById(id);
+//		bookVO.add(linkTo(methodOn(BookController.class).findById(id)).withSelfRel());
+		return bookVO;
 	}
 	
-	@ApiOperation(value = "Create a people")
+	@ApiOperation(value = "Create a book record")
 	@PostMapping(produces = {"application/json","application/xml", "application/x-yaml"},
 			consumes = {"application/json", "application/xml", "application/x-yaml"})
-	public PersonVO create(@RequestBody PersonVO PersonVO) {
+	public BookVO create(@RequestBody BookVO BookVO) {
 		
-		return personService.create(PersonVO);
+		return bookService.create(BookVO);
 		
 	}
 	
-	@ApiOperation(value = "Create a people V2")
+	@ApiOperation(value = "Find all books recorded V2")
 	@PostMapping(value = "v2", produces = {"application/json","application/xml", "application/x-yaml"},
 			consumes = {"application/json", "application/xml", "application/x-yaml"})
-	public PersonVO createv2(@RequestBody PersonVO PersonVO) {
+	public BookVO createv2(@RequestBody BookVO BookVO) {
 		
-		return personService.create(PersonVO);
+		return bookService.create(BookVO);
 		
 	}
 	
 	
 	@PutMapping(produces = {"application/json","application/xml", "application/x-yaml"},
 			consumes = {"application/json", "application/xml", "application/x-yaml"})
-	public PersonVO update(@RequestBody PersonVO PersonVO) {
+	public BookVO update(@RequestBody BookVO BookVO) {
 		
-		return personService.update(PersonVO);
+		return bookService.update(BookVO);
 		
 	}
 	
 	@DeleteMapping("/{id}")
 	public ResponseEntity<?> delete(@PathVariable("id") Long id) {
 		
-		personService.delete(id);
+		bookService.delete(id);
 		return ResponseEntity.ok().build();
 		
 	}
